24c4904ac810f3f2ac61c76ff95cc705e341f85e
Orxan477 <tu201906147@code.edu.az>
2022-09-13 16:40:16 +0400

    Home VM Created

---
+++ ElvinExam/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using ElvinExam.Models;
+
+namespace ElvinExam.ViewModels
+{
+    public class HomeVM
+    {
+        List<Subjects> Subjects { get; set; }
+        List<Paids> Paids { get; set; }
+        List<Months> Months { get; set; }
+    }
+}
