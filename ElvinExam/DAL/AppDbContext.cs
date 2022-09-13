d275572371c2ecbb3c3f8e3885fbe6af0ce22841
Orxan477 <tu201906147@code.edu.az>
2022-09-13 16:30:04 +0400

    Dal folder and program.cs file done

--- ElvinExam/DAL/AppDbContext.cs
+++ ElvinExam/DAL/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ElvinExam.DAL
+﻿using ElvinExam.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElvinExam.DAL
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+
+        public DbSet<Months> Months { get; set; }
+        public DbSet<Subjects> Subjects { get; set; }
+        public DbSet<Paids> Paids { get; set; }
+        public DbSet<Setting>Settings { get; set; }
     }
 }
--- ElvinExam/Program.cs
+++ ElvinExam/Program.cs
@@ -1,7 +1,15 @@
+using ElvinExam.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:Default"]);
+});
 
 var app = builder.Build();
 
@@ -12,6 +20,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
