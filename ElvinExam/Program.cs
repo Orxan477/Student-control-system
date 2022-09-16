53723b06dca601ca3e5e0788ac4e438baa8ac27a
Orxan477 <tu201906147@code.edu.az>
2022-09-16 23:55:22 +0400

    Area added

--- ElvinExam/Program.cs
+++ ElvinExam/Program.cs
@@ -38,4 +38,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
+
