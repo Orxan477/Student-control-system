38157745e0661f4b5f77e8c51f569cc4e14d0de2
Orxan477 <tu201906147@code.edu.az>
2022-09-22 11:56:08 +0400

    Repository Created

---
+++ ControlSystem.Core/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ControlSystem.Core.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<T> GetAll();
+        Task CreatePaid(int id);
+        Task UpdatePaid(int id);
+    }
+}
---
+++ ControlSystem.Data/Implementations/Repository.cs
@@ -0,0 +1,22 @@
+﻿using ControlSystem.Core.Interfaces;
+
+namespace ControlSystem.Data.Implementations
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        public Task<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public Task CreatePaid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePaid(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
