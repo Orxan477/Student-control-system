2273065f55a61e9971e2c2a8c954060a3413932c
Orxan477 <tu201906147@code.edu.az>
2022-09-24 00:44:37 +0400

    Created IExamOfWork

---
+++ ControlSystem.Business/Implementations/ExamOfWork.cs
@@ -0,0 +1,20 @@
+﻿using ControlSystem.Business.Interfaces;
+using ControlSystem.Core.Interfaces;
+
+namespace ControlSystem.Business.Implementations
+{
+    public class ExamOfWork : IExamOfWork
+    {
+        private readonly PaidService _paidService;
+        private readonly HomeService _homeService;
+        private IUnitOfWork _unitOfWork;
+
+        public ExamOfWork(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IPaidService PaidService => _paidService ?? new PaidService(_unitOfWork);
+
+        public IHomeService HomeService => _homeService ?? new HomeService(_unitOfWork);
+    }
+}
---
+++ ControlSystem.Business/Interfaces/IExamOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace ControlSystem.Business.Interfaces
+{
+    internal interface IExamOfWork
+    {
+        public IPaidService PaidService { get; }
+        public IHomeService HomeService { get; }
+    }
+}
